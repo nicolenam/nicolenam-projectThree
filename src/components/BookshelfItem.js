5efad177fbfd97ac09918b9b29e8287bc19b629e
nicole-nam <eunjungnam@gmail.com>
2023-05-11 17:26:44 -0400

    Deciphered the reason why bookArray cannot be removed in bookshelfItem

--- src/components/BookshelfItem.js
+++ src/components/BookshelfItem.js
@@ -2,7 +2,19 @@ import { Link } from "react-router-dom";
 import addBtn from "../assets/plus-icon.png";
 import minusBtn from "../assets/minusBtn.png";
 
-const BookshelfItem = ({color, bookArray}) =>{
+const BookshelfItem = ({color, bookArray, setBookArray }) =>{
+
+    const handleClick = (e) =>{
+        const removeBook = e.target.alt;
+        console.log('book array:',bookArray);
+        console.log("remove book:", removeBook);
+        //filter removeBook from bookArray and setBookArray with updated bookArray
+        // const updatedBookArray = bookArray.filter((book)=> book !== removeBook);
+        // console.log(updatedBookArray);
+        // setBookArray(updatedBookArray);
+    }
+
+    //!!!! must move this logic into bookshelf, because that's where i have the actual array. 
 
     return(
         <div className={`bookshelf-item ${color}-grid`} >
@@ -14,7 +26,7 @@ const BookshelfItem = ({color, bookArray}) =>{
                         book?
                             <>
                                 <img className="bookshelf-book" src={book} alt="book url" />
-                                <img className="remove-book" src={minusBtn} alt="book url" />
+                                <img className="remove-book" onClick={handleClick} alt={book} src={minusBtn} />
                             </>
                         :
                         <Link to="/category">
